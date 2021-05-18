ae11d98ff4318ff7063eea66cb73289b3b5d2b28
Sergey Pavlov <self@sergeypavlov.dev>
2021-05-18 23:38:10 +0300

    Fix default value for Room.code

--- app-django/api/models.py
+++ app-django/api/models.py
@@ -3,16 +3,18 @@ from django.utils.crypto import get_random_string
 
 
 class Room(models.Model):
-    code = models.CharField(max_length=8, default='', unique=True)
-    host = models.CharField(max_length=50, unique=True)
-    guest_can_pause = models.BooleanField(null=False, default=False)
-    votes_to_skip = models.IntegerField(null=False, default=2)
-    created_at = models.DateTimeField(auto_now_add=True)
-    
-    def generate_unique_code():
+
+    def unique_code():
         while True:
             code = get_random_string(length=6)
             if not Room.objects.filter(code=code).exists():
                 break
         return code
+
+    code = models.CharField(max_length=8, default=unique_code, unique=True)
+    host = models.CharField(max_length=50, unique=True)
+    guest_can_pause = models.BooleanField(null=False, default=False)
+    votes_to_skip = models.IntegerField(null=False, default=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
         
