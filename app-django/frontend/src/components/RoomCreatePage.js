6032b74bbd823086ad2de83cff8d8e76ece3dd69
Sergey Pavlov <self@sergeypavlov.dev>
2021-05-19 17:24:24 +0300

    Update Room creation page

--- app-django/frontend/src/components/RoomCreatePage.js
+++ app-django/frontend/src/components/RoomCreatePage.js
@@ -1,8 +1,74 @@
-import React from "react";
+import React, { Component } from "react";
+import {
+    Button,
+    Grid,
+    Typography,
+    TextField,
+    FormHelperText,
+    FormControl,
+    FormControlLabel,
+    Radio,
+    RadioGroup
+} from '@material-ui/core';
+import { Link } from "react-router-dom";
 
 
-export default function RoomCreatePage() {
-    return (
-        <p>This is the create room page</p>
-    );
+export default class RoomCreatePage extends Component {
+    defaultVotes = 2;
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Grid container spacing={1}>
+                <Grid item xs={12} align="center">
+                    <Typography component="h4" variant="h4">
+                        Create a Room
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <FormControl component="fieldset">
+                        <FormHelperText>
+                            <div align="center">
+                                Guest Control of Playback State
+                            </div>
+                        </FormHelperText>
+                        <RadioGroup row defaultValue="true">
+                            <FormControlLabel
+                                value="true"
+                                control={<Radio color="primary" />}
+                                label="Play/Pause"
+                                labelPlacement="bottom"
+                            />
+                            <FormControlLabel
+                                value="false"
+                                control={<Radio color="secondary" />}
+                                label="No Control"
+                                labelPlacement="bottom"
+                            />
+                        </RadioGroup>
+                    </FormControl>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <FormControl>
+                        <TextField
+                            required={true}
+                            type="number"
+                            defaultValue={this.defaultVotes}
+                            inputProps={{
+                                min: 1
+                            }}
+                        />
+                        <FormHelperText>
+                            <div align="center">
+                                Votes required to skip song
+                            </div>
+                        </FormHelperText>
+                    </FormControl>
+                </Grid>
+            </Grid>
+        )
+    }
 }
