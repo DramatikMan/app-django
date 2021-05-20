7c1d830a3945f50de3115f88c6596109872f2950
Sergey Pavlov <self@sergeypavlov.dev>
2021-05-20 16:15:34 +0300

    Update Home page

--- app-django/frontend/src/components/HomePage.js
+++ app-django/frontend/src/components/HomePage.js
@@ -1,8 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Grid, Button, ButtonGroup, Typography } from "@material-ui/core";
 
 
 export default function HomePage() {
     return (
-        <p>This is the home page</p>
+        <Grid
+        container
+        spacing={3}
+        direction="column"
+        alignItems="center"
+        justify="center"
+        style={{ minHeight: "90vh" }}
+        >
+            <Grid item xs={12}>
+                <Typography variant="h3" compact="h3">
+                    Music App
+                </Typography>
+            </Grid>
+            <Grid item xs={12}>
+                <ButtonGroup
+                    disableElevation
+                    variant="contained"
+                    color="primary"
+                >
+                    <Button color="primary" to="/join" component={ Link }>
+                        Join a Room
+                    </Button>
+                    <Button color="secondary" to="/create" component={ Link }>
+                        Create a Room
+                    </Button>
+                </ButtonGroup>
+            </Grid>
+        </Grid>
     );
 }
