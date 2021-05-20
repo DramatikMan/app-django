599b298cb1bdd521fd2f629356675f295a22d196
Sergey Pavlov <self@sergeypavlov.dev>
2021-05-20 22:14:44 +0300

    Update Room page

--- app-django/frontend/src/components/Room.js
+++ app-django/frontend/src/components/Room.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import { Button, Grid, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 
 export default function Room(props) {
@@ -18,11 +20,44 @@ export default function Room(props) {
     useEffect(() => { getRoomDetails(); }, []);
 
     return (
-        <div>
-            <h3>{roomCode}</h3>
-            <p>Votes: {votesToSkip}</p>
-            <p>Guest Can Pause: {guestCanPause.toString()}</p>
-            <p>Host: {isHost.toString()}</p>
-        </div>
+        <Grid
+        container
+        spacing={1}
+        direction="column"
+        alignItems="center"
+        justify="center"
+        style={{ minHeight: "90vh" }}
+        >
+            <Grid item xs={12}>
+                <Typography variant="h6" component="h6">
+                    Room Code: {roomCode}
+                </Typography>
+            </Grid>
+            <Grid item xs={12}>
+                <Typography variant="h6" component="h6">
+                    Votes: {votesToSkip}
+                </Typography>
+            </Grid>
+            <Grid item xs={12}>
+                <Typography variant="h6" component="h6">
+                    Guest Can Pause: {guestCanPause.toString()}
+                </Typography>
+            </Grid>
+            <Grid item xs={12}>
+                <Typography variant="h6" component="h6">
+                    Host: {isHost.toString()}
+                </Typography>
+            </Grid>
+            <Grid item xs={12}>
+                <Button
+                    variant="contained"
+                    color="secondary"
+                    to="/"
+                    component={Link}
+                >
+                    Leave Room
+                </Button>
+            </Grid>
+        </Grid>
     )
 }
