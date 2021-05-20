24b84b41b29c331951e9134a5e2c79f3eb743b83
Sergey Pavlov <self@sergeypavlov.dev>
2021-05-20 21:37:37 +0300

    Update Home page

--- app-django/frontend/src/components/HomePage.js
+++ app-django/frontend/src/components/HomePage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { Grid, Button, ButtonGroup, Typography } from "@material-ui/core";
 
