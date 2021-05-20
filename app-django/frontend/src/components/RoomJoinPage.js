bf34b8f579da4f96e2d1017900149cf0035fdec5
Sergey Pavlov <self@sergeypavlov.dev>
2021-05-20 12:32:44 +0300

    Sketch out Room join page

--- app-django/frontend/src/components/RoomJoinPage.js
+++ app-django/frontend/src/components/RoomJoinPage.js
@@ -1,8 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+    Button,
+    Grid,
+    Typography,
+    TextField
+} from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 
 export default function RoomJoinPage() {
+    const [roomCode, setRoomCode] = useState();
+    const [error, setError] = useState(false);
+
     return (
-        <p>This is the room join page</p>
+        <Grid
+        container
+        spacing={1}
+        direction="column"
+        alignItems="center"
+        justify="center"
+        style={{ minHeight: "90vh" }}
+        >
+            <Grid item xs={12}>
+                <Typography variant="h4" component="h4">
+                    Join a Room
+                </Typography>
+            </Grid>
+            <Grid item xs={12}>
+                <TextField
+                    error={error}
+                    label="Code"
+                    placeholder="Enter a Room Code"
+                    value={roomCode}
+                    helperText={error}
+                    variant="outlined"
+                    onChange={(e) => setRoomCode(e.target.value)}
+                />
+            </Grid>
+            <Grid item xs={12}>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={() => console.log(roomCode)}
+                >
+                    Enter Room
+                </Button>
+            </Grid>
+            <Grid item xs={12}>
+                <Button
+                    variant="contained"
+                    color="secondary"
+                    to="/"
+                    component={Link}
+                >
+                    Back
+                </Button>
+            </Grid>
+        </Grid>
     );
 }
