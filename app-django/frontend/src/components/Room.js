a8b25e4a69065b0b422e6f493ba9782009cd3d0b
Sergey Pavlov <self@sergeypavlov.dev>
2021-05-20 22:07:23 +0300

    Transform into function Component

--- app-django/frontend/src/components/Room.js
+++ app-django/frontend/src/components/Room.js
@@ -1,37 +1,28 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 
 
-export default class Room extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            votesToSkip: 2,
-            guestCanPause: false,
-            isHost: false
-        };
-        this.roomCode = this.props.match.params.roomCode;
-        this.getRoomDetails();
+export default function Room(props) {
+    const roomCode = props.match.params.roomCode;
+    const [votesToSkip, setVotesToSkip] = useState(2);
+    const [guestCanPause, setGuestCanPause] = useState(false);
+    const [isHost, setIsHost] = useState(false);
+
+    async function getRoomDetails() {
+        let response = await fetch("/api/get-room" + "?code=" + roomCode);
+        let responseData = await response.json();
+        setVotesToSkip(responseData.votes_to_skip);
+        setGuestCanPause(responseData.guest_can_pause);
+        setIsHost(responseData.is_host);
     }
 
-    getRoomDetails() {
-        fetch("/api/get-room" + "?code=" + this.roomCode)
-            .then(response => response.json())
-            .then(data => { this.setState({
-                votesToSkip: data.votes_to_skip,
-                guestCanPause: data.guest_can_pause,
-                isHost: data.is_host
-            });
-        });
-    }
+    useEffect(() => { getRoomDetails(); }, []);
 
-    render() {
-        return (
-            <div>
-                <h3>{this.roomCode}</h3>
-                <p>Votes: {this.state.votesToSkip}</p>
-                <p>Guest Can Pause: {this.state.guestCanPause.toString()}</p>
-                <p>Host: {this.state.isHost.toString()}</p>
-            </div>
-        )
-    }
+    return (
+        <div>
+            <h3>{roomCode}</h3>
+            <p>Votes: {votesToSkip}</p>
+            <p>Guest Can Pause: {guestCanPause.toString()}</p>
+            <p>Host: {isHost.toString()}</p>
+        </div>
+    )
 }
