94f70978c73421767d784e5cbaad3258f536345e
Sergey Pavlov <self@sergeypavlov.dev>
2021-05-20 16:58:57 +0300

    Add "user in Room" check view

--- app-django/api/views.py
+++ app-django/api/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from rest_framework import generics, status
 from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework.response import response
 
 from .models import Room
 from .serializers import RoomSerializer, RoomCreateSerializer
@@ -104,3 +105,15 @@ class CreateRoomView(APIView):
             {'Bad Request': 'Invalid data.'},
             status=status.HTTP_400_BAD_REQUEST
         )
+
+
+class UserInRoom(APIView):
+    def get(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        data = {
+            'code': self.request.session.get('room_code')
+        }
+
+        return JsonResponse(data, status=status.HTTP_200_OK)
