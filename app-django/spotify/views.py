598a9300ede86c0677002c031626b86f42460a8c
Sergey Pavlov <self@sergeypavlov.dev>
2021-05-22 14:22:29 +0300

    Add view for constructing Spotify auth request URL

--- app-django/spotify/views.py
+++ app-django/spotify/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+import os
+import requests
 
-# Create your views here.
+from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class AuthURL(APIView):
+    def get(self, request, format=None):
+        scopes = (
+            'user-read-playback-state',
+            'user-modify-playback-state',
+            'user-read-currently-playing'
+        )
+        payload = dict(
+            scope=' '.join(scopes),
+            response_type='code',
+            redirect_uri=os.environ['SPOTIFY_REDIRECT_URI'],
+            client_id=os.environ['SPOTIFY_CLIENT_ID']
+        )
+        url = requests.Request(
+            'GET',
+            'https://accounts.spotify.com/authorize',
+            params=payload
+        ).prepare().url
+
+        return Response({'url': url}, status=status.HTTP_200_OK)
+
