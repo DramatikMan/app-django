b98192b8357f88b16eda444c83e8ee8602416523
Sergey Pavlov <self@sergeypavlov.dev>
2021-05-24 17:58:46 +0300

    Extend tokens field length

--- music_app/spotify/models.py
+++ music_app/spotify/models.py
@@ -5,9 +5,9 @@ from django.db import models
 class SpotifyToken(models.Model):
     user = models.CharField(max_length=50, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    access_token = models.CharField(max_length=150)
+    access_token = models.CharField(max_length=255)
     token_type = models.CharField(max_length=50)
-    refresh_token = models.CharField(max_length=150)
+    refresh_token = models.CharField(max_length=255)
     expiry_dt = models.DateTimeField()
 
 
