62c36e5b131c06c4da87e3b99176fdec098432ee
Sergey Pavlov <self@sergeypavlov.dev>
2021-05-27 13:38:42 +0300

    Create About.js

---
+++ music_app/frontend/src/components/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+
+
+export default function About() {
+    const history = useHistory();
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
