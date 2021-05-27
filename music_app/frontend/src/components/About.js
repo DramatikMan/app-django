786630fe3b2963bd772aae94d8f44d999c222d9d
Sergey Pavlov <self@sergeypavlov.dev>
2021-05-27 15:17:46 +0300

    Update About page

--- music_app/frontend/src/components/About.js
+++ music_app/frontend/src/components/About.js
@@ -1,13 +1,43 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import { Grid, Button, Typography } from "@material-ui/core";
 
 
 export default function About() {
     const history = useHistory();
 
     return (
-        <div>
-            
-        </div>
+        <Grid
+            container
+            spacing={3}
+            direction="column"
+            alignItems="center"
+            justify="center"
+            style={{ minHeight: "90vh" }}
+        >
+            <Grid item xs={12} align="center">
+                <Typography variant="body1">
+                    This app allows you to host or join rooms and contol the host's Spotify playback.
+                </Typography>
+                <Typography variant="body1">
+                    The host needs to start playing something from their Spotify account for this to work.
+                </Typography>
+                <Typography variant="body1">
+                    Also the host's Spotify account has to have Premium. Their rules, not mine ¯\_(ツ)_/¯ .
+                </Typography>
+            </Grid>
+            <Grid item xs={12} align="center">
+                <Typography variant="body1">
+                    <Button
+                        color="secondary"
+                        variant="contained"
+                        to="/" 
+                        component={Link}
+                    >
+                        back
+                    </Button>
+                </Typography>
+            </Grid>
+        </Grid>
     );
 }
