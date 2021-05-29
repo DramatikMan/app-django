1dfb0750c16c30483e879c85aec4a7d97d80ed1e
Sergey Pavlov <self@sergeypavlov.dev>
2021-05-29 18:10:49 +0300

    Apply MUI dark palette

--- music_app/frontend/src/index.js
+++ music_app/frontend/src/index.js
@@ -1,7 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ThemeProvider } from "@material-ui/styles";
+import { createMuiTheme, CssBaseline } from "@material-ui/core";
 
 import App from "./components/App";
 
 
-ReactDOM.render(<App />, document.getElementById("app"));
+const darkTheme = createMuiTheme({
+    palette: {
+      type: "dark",
+    },
+  });
+
+ReactDOM.render(
+    <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <App />
+    </ThemeProvider>,
+    document.getElementById("app")
+);
