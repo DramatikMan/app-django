26f6366dcb8f0badd91ca983e49b7b3229250601
Sergey Pavlov <self@sergeypavlov.dev>
2021-08-10 21:26:45 +0300

    Add 'HomePage' component

---
+++ app/frontend/static/src/components/HomePage.tsx
@@ -0,0 +1,33 @@
+import { FC } from 'react';
+import { Link } from 'react-router-dom';
+import { Grid, ButtonGroup, Button } from '@material-ui/core';
+
+
+const HomePage: FC = (): JSX.Element => {
+  return (
+    <Grid container
+      direction='column'
+      alignItems='center'
+      justifyContent='center'
+      style={{ minHeight: '90vh' }}
+      spacing={3}
+    >
+      <Grid item xs={12}>
+        <ButtonGroup disableElevation
+          variant='contained'
+        >
+          <Button
+            color='default'
+            to='/about'
+            component={Link}  
+          >
+            About
+          </Button>
+        </ButtonGroup>
+      </Grid>
+    </Grid>
+  );
+}
+
+
+export default HomePage;
