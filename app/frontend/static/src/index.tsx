68f49097172831ec6146f5dfc6221cae00b3e842
Sergey Pavlov <self@sergeypavlov.dev>
2021-08-13 22:38:00 +0300

    Connect Redux DevTools

--- app/frontend/static/src/index.tsx
+++ app/frontend/static/src/index.tsx
@@ -3,13 +3,14 @@ import { CssBaseline } from '@material-ui/core';
 import { createTheme, ThemeProvider, Theme } from '@material-ui/core/styles';
 import { createStore, Store } from 'redux';
 import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import App from './components/App';
 import reducer from './reducer';
 
 
 const darkTheme: Theme = createTheme({ palette: { type: 'dark' } });
-const store: Store = createStore(reducer);
+const store: Store = createStore(reducer, composeWithDevTools());
 
 
 ReactDOM.render(
