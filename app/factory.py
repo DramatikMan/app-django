60122073dee51b3653c45d138a3168fd1909a817
Sergey Pavlov <self@sergeypavlov.dev>
2021-08-14 20:25:53 +0300

    Set log level to DEBUG & add aiohttp session

--- app/factory.py
+++ app/factory.py
@@ -1,8 +1,14 @@
+import base64
+import logging
 from pathlib import Path
 
-from aiohttp import web
 import jinja2
 import aiohttp_jinja2
+from aiohttp import web
+from aiohttp.web_response import Response
+from aiohttp_session import Session, setup, get_session
+from aiohttp_session.cookie_storage import EncryptedCookieStorage
+from cryptography import fernet
 
 from .routers.api import app as api
 
@@ -21,11 +27,19 @@ async def root(request: web.Request) -> dict[str, str]:
 
 def create_app() -> web.Application:
     app = web.Application()
+    logging.basicConfig(level=logging.DEBUG)
 
+    # aiohttp session
+    fernet_key: bytes = fernet.Fernet.generate_key()
+    secret_key: bytes = base64.urlsafe_b64decode(fernet_key)
+    setup(app, EncryptedCookieStorage(secret_key))
+
+    # routes
     app.add_routes(routes)
     app.add_subapp('/api/', api)
-
     app.add_routes([web.static('/static', static_path)])
+
+    # markup template loader
     aiohttp_jinja2.setup(app, loader=loader)
 
     return app
