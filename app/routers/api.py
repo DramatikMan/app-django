dae02354398ef05ff69d72a90a88ed22e46656f3
Sergey Pavlov <self@sergeypavlov.dev>
2021-08-15 17:03:41 +0300

    Add Room creation functionality

--- app/routers/api.py
+++ app/routers/api.py
@@ -1,8 +1,20 @@
+from datetime import datetime
+from typing import TypedDict
+
 from aiohttp import web
 from aiohttp.web_response import Response
+from aiohttp_session import get_session, Session
+
+from ..db.models import Room
+from ..db.config import Session as DatabaseSession
 
 
 routes = web.RouteTableDef()
+
+
+class PostRoomReqData(TypedDict):
+    guestCanPause: bool
+    votesToSkip: int
 
 
 @routes.get('/room')
@@ -12,7 +24,27 @@ async def get_rooms(request: web.Request) -> Response:
 
 @routes.post('/room')
 async def create_room(request: web.Request) -> Response:
-    return web.json_response({'status': 'OK'})
+    data: PostRoomReqData = await request.json()
+    guest_can_pause: bool = data['guestCanPause']
+    votes_to_skip: int = data['votesToSkip']
+
+    session: Session = await get_session(request)
+    host: str = session.identity
+
+    with DatabaseSession() as db_session:
+        room = Room(
+            host=host,
+            guest_can_pause=guest_can_pause,
+            votes_to_skip=votes_to_skip,
+            created_at=datetime.now()
+        )
+
+        db_session.add(room)
+        db_session.commit()
+
+        code: str = room.code
+
+    return web.json_response({'code': code})
 
 
 app = web.Application()
