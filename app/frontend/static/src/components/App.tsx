dc5ba86c82f0bd68b3d4a5b980cd7fbe5eec8007
Sergey Pavlov <self@sergeypavlov.dev>
2021-08-18 18:50:05 +0300

    Add '/join' Route for RoomJoinPage component

--- app/frontend/static/src/components/App.tsx
+++ app/frontend/static/src/components/App.tsx
@@ -5,6 +5,7 @@ import HomePage from './HomePage';
 import About from './About';
 import RoomSettingsPage from './RoomSettingsPage';
 import RoomPage from './RoomPage';
+import RoomJoinPage from './RoomJoinPage';
 
 
 const App: FC = (): JSX.Element => {
@@ -15,6 +16,7 @@ const App: FC = (): JSX.Element => {
         <Route exact path='/about' component={About} />
         <Route exact path='/create' component={RoomSettingsPage} />
         <Route exact path='/room/:roomCode' component={RoomPage} />
+        <Route exact path='/join' component={RoomJoinPage} />
       </Switch>
     </Router>
   );
