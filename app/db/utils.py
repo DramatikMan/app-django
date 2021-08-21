303707c92ac1c7cdd98653ff07faf98b75ffe814
Sergey Pavlov <self@sergeypavlov.dev>
2021-08-21 19:27:20 +0300

    Simplify update_or_create_token function

--- app/db/utils.py
+++ app/db/utils.py
@@ -1,4 +1,4 @@
-from typing import Optional, Any
+from typing import Optional
 from datetime import timedelta, datetime
 
 from sqlalchemy.orm.query import Query
@@ -23,17 +23,12 @@ def update_or_create_tokens(
     identity: str,
     data: SpotifyAuthResponseData
 ) -> None:
-    access_token: str = data.get('access_token')
-    token_type: str = data.get('token_type')
-    expires_in: Any = data.get('expires_in')
-    refresh_token: str = data.get('refresh_token')
-
     with Session() as session:
         session.merge(SpotifyTokens(
             user=identity,
-            access_token=access_token,
-            token_type=token_type,
-            refresh_token=refresh_token,
-            expiry_dt=datetime.now() + timedelta(seconds=expires_in)
+            access_token=data['access_token'],
+            token_type=data['token_type'],
+            refresh_token=data['refresh_token'],
+            expiry_dt=datetime.now() + timedelta(seconds=data['expires_in'])
         ))
         session.commit()
