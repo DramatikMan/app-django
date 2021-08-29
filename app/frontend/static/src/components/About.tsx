d969b60095f338f967ad174a189e586a71a1715b
Sergey Pavlov <self@sergeypavlov.dev>
2021-08-29 17:38:10 +0300

    Update About page text content

--- app/frontend/static/src/components/About.tsx
+++ app/frontend/static/src/components/About.tsx
@@ -14,13 +14,13 @@ const About: FC = (): JSX.Element => {
     >
       <Grid item xs={12}>
         <Typography variant='body1' align='center'>
-          This app allows you to host or join rooms to control the host's Spotify playback.
+          This app allows you to host or join rooms to control host's Spotify playback.
         </Typography>
         <Typography variant='body1' align='center'>
-          The host needs to start playing something from their Spotify account for this to work.
+          Room host needs to start playing something from their Spotify account for this to work.
         </Typography>
         <Typography variant='body1' align='center'>
-          Also the host's Spotify account has to have Premium. Their rules, not mine ¯\_(ツ)_/¯ .
+          Also host's Spotify account has to have Premium. Their rules, not mine ¯\_(ツ)_/¯ .
         </Typography>
       </Grid>
       <Grid item xs={12}>
