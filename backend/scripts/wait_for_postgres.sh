5637c4962150292ff2798bc0f9c2c09ceb70e534
Sergey Pavlov <self@sergeypavlov.dev>
2022-04-08 14:16:15 +0300

    Update environment

--- backend/scripts/wait_for_postgres.sh
+++ backend/scripts/wait_for_postgres.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 set -e
 
-until poetry run python scripts/check_postgres.py &> /dev/null; do
+until python3 -c "
+import os
+import socket
+
+
+if __name__ == '__main__':
+    try:
+        sock = socket.create_connection((
+            os.environ['POSTGRES_HOST'],
+            os.environ['POSTGRES_PORT']
+        ))
+    except Exception:
+        raise
+    else:
+        sock.close()
+" &> /dev/null; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
