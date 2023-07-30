6115b5e5f6111f5829b9bf89be481b8af39bc882
Sergey Pavlov <self@sergeypavlov.dev>
2023-07-30 21:22:29 +0300

    Add home page

---
+++ src/app/theme.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import {ThemeProvider, createTheme} from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const darkTheme = createTheme({
+    palette: {
+        mode: "dark",
+    },
+});
+
+function DarkTheme({children}: {children: React.ReactNode}) {
+    return (
+        <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            {children}
+        </ThemeProvider>
+    );
+}
+
+export default DarkTheme;
